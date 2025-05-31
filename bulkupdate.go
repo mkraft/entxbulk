40291e0dca952891ae1722fc96c42313c7d82446
Martin Kraft <martin@upspin.org>
2025-05-31 13:08:23 -0400

    Initial commit.

---
+++ bulkupdate.go
@@ -0,0 +1,79 @@
+package entxbulk
+
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"text/template"
+
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+type Option func(*Extension)
+
+type Extension struct {
+	typeCasts map[string]string
+}
+
+func WithTypeCasts(casts map[string]string) Option {
+	return func(e *Extension) {
+		e.typeCasts = casts
+	}
+}
+
+func NewExtension(opts ...Option) Extension {
+	e := Extension{
+		typeCasts: make(map[string]string),
+	}
+	for _, opt := range opts {
+		opt(&e)
+	}
+	return e
+}
+
+func (e Extension) Templates() []*gen.Template {
+	_, currentFile, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(currentFile)
+	templatePath := filepath.Join(dir, "template", "bulk_update.tmpl")
+
+	log.Printf("Loading template from: %s", templatePath)
+
+	t, err := gen.NewTemplate("bulk_update").
+		Funcs(template.FuncMap{
+			"ToExported": func(s string) string {
+				if s == "" {
+					return ""
+				}
+				return strings.Title(s)
+			},
+			"until": func(n int) []int {
+				a := make([]int, n)
+				for i := range a {
+					a[i] = i
+				}
+				return a
+			},
+		}).
+		ParseFiles(templatePath)
+	if err != nil {
+		log.Printf("Failed to parse template: %v", err)
+		panic(err)
+	}
+
+	log.Printf("Successfully loaded template")
+	return []*gen.Template{t}
+}
+
+func (Extension) Hooks() []gen.Hook {
+	return nil
+}
+
+func (Extension) Annotations() []entc.Annotation {
+	return nil
+}
+
+func (Extension) Options() []entc.Option {
+	return nil
+}
